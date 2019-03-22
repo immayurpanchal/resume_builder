6617df64b0c99a59aa816662aa6adece3b09a86c
immayurpanchal <mayurpanchal002@gmail.com>
2019-03-22 18:28:58 +0530

    Added rsuiteJs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button } from 'rsuite';
 import './App.css';
+import 'rsuite/dist/styles/rsuite.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Button color="primary">Hello World</Button>
       </div>
     );
   }
